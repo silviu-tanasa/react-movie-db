f4865c186b1e75c531f5843e3772c215aaaa6ef0
silviu-tanasa <37173637+silviu-tanasa@users.noreply.github.com>
2018-08-27 17:15:19 +0300

    Tried to create reusable code

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import MovieItem from './MovieItem';
-import MoviesStyle from './MoviesList.css';
+import './MoviesList.css';
 
-function MovieList() {
-    return(
-        <div className="movie-list">
-            <div className="movies-container">
-            <MovieItem 
-                title="Lord of the Rings"
-                year="2001"
-                desc="A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron."
-            />
+class MovieList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listOfMovies: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/movies').then((response) => {
+            console.log(response.data);
+            this.setState({
+                listOfMovies: response.data,
+            });
+            
+        })
+    }
+
+    // listMovies() {
+    //     for(let i = 0; i < this.state.listOfMovies.length; i++)
+    //         console.log(this.state.listOfMovies[i].title)
+    // }
+
+    render() {
+        return(
+            <div className="movie-list">
+                <div className="movies-container">
+                
+                <MovieItem 
+                    title="{this.state.listOfMovies}"
+                    year="2001"
+                    desc="A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron."
+                />
+                </div>
+               <Button variant="contained" color="primary">
+                    Add New Movie
+                </Button> 
             </div>
-           <Button variant="contained" color="primary">
-                Add New Movie
-            </Button> 
-        </div>
-    )
+        )
+    }
+    
 }
 export default MovieList;
